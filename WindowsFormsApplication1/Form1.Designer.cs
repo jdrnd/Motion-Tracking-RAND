f5e84b8b011d66ad2bdf90a436f0b9856fdd55dd
underdiver <jruhls@gmail.com>
2014-10-13 19:47:00 -0400

    Added current project code to Github
    Gets webcam image, displays and plays back. Some motion tracking done

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stream = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.Analyse = new System.Windows.Forms.Button();
+            this.colordata = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.stream)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // stream
+            // 
+            this.stream.Location = new System.Drawing.Point(13, 13);
+            this.stream.Name = "stream";
+            this.stream.Size = new System.Drawing.Size(320, 240);
+            this.stream.TabIndex = 0;
+            this.stream.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 274);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(93, 274);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(75, 23);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // Analyse
+            // 
+            this.Analyse.Location = new System.Drawing.Point(174, 274);
+            this.Analyse.Name = "Analyse";
+            this.Analyse.Size = new System.Drawing.Size(75, 23);
+            this.Analyse.TabIndex = 3;
+            this.Analyse.Text = "Analyse";
+            this.Analyse.UseVisualStyleBackColor = true;
+            this.Analyse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // colordata
+            // 
+            this.colordata.FormattingEnabled = true;
+            this.colordata.Location = new System.Drawing.Point(366, 13);
+            this.colordata.Name = "colordata";
+            this.colordata.Size = new System.Drawing.Size(248, 238);
+            this.colordata.TabIndex = 4;
+            this.colordata.SelectedIndexChanged += new System.EventHandler(this.colordata_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 330);
+            this.Controls.Add(this.colordata);
+            this.Controls.Add(this.Analyse);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.stream);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.stream)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox stream;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button Analyse;
+        private System.Windows.Forms.ListBox colordata;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+//Imports AForge libraries
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Diagnostics;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<Bitmap> frames = new List<Bitmap>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Create webcam object
+        VideoCaptureDevice videoSource;
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //List all available video sources
+            FilterInfoCollection videosources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videosources != null)
+            {
+
+                //Use first video source
+                videoSource = new VideoCaptureDevice(videosources[0].MonikerString);
+
+                try
+                {
+                    videoSource.VideoResolution = videoSource.VideoCapabilities[2];  
+                }
+                
+
+                catch { }
+
+                //Create NewFrame event handler
+                //(This one triggers every time a new frame/image is captured
+                videoSource.NewFrame += new AForge.Video.NewFrameEventHandler(videoSource_NewFrame);
+
+                //Start recording
+                videoSource.Start();
+            }
+        }
+        void videoSource_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            //Cast the frame as a Bitmap object  
+            
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+
+            stream.BackgroundImage = bitmap;
+
+            frames.Add(bitmap);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            videoSource.SignalToStop();
+            videoSource = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stream.BackgroundImage = null;
+
+            foreach (Bitmap bitmap in frames)
+            {
+                stream.BackgroundImage = bitmap;
+
+                Application.DoEvents();
+
+                //Goes through image as a pixel array and assigns number values to rgb colors
+
+                int[,] data = new int[320, 240];
+
+                for (int i = 0; i < 320; i++)
+                {
+                    for (int j = 0; j < 240; j++)
+                    {
+                        Color Color = bitmap.GetPixel( i, j);
+                        if (Color.R > 0.5f && Color.R < 1f)
+                        {
+                            data[i, i] = 1;
+                        }
+                        else if (Color.G > 0.5f && Color.G < 1f)
+                        {
+                            data[i, j] = 2;
+                        }
+                        else if (Color.B > 0.5f && Color.B < 1f)
+                        {
+                            data[i, j] = 3;
+                            Console.Write("Blue");
+                        }
+                        else
+                        {
+                            data[i, j] = 0;
+                            
+                        }
+
+                    }
+                }
+
+                //Passes colored pixels to array display box
+
+                for (int i = 0; i < 320; i++)
+                {
+
+                    for (int j = 0; j < 240; j++)
+                    {
+
+                        if (data[i, j] > 0)
+                        {
+                            colordata.Items.Add(data[i, j].ToString());
+                        }
+                    }
+
+                }
+            }
+        }
+        private void colordata_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+ 
+        
+
+    }
+}
